317737b3ead144dc9ef6b92d70c2059c6e135699
Lemsisest <toktorbekov_m@auca.kg>
2024-11-14 10:48:02 +0600

    commit your react-lesson2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import {ProductCard} from "./components/ProductCard";
 
+const products = [
+  {
+    id: 0,
+    name: "Product 1",
+    price: 100,
+    category: "Category 1",
+    image: "https://hips.hearstapps.com/hmg-prod/images/magical-halloween-night-the-full-moon-illuminates-royalty-free-image-1729099355.jpg"
+  },
+  {
+    id: 1,
+    name: "Product 2",
+    price: 200,
+    category: "Category 1",
+    image: "https://www.astronomy.com/uploads/2023/09/underfullmoon.jpeg"
+
+  }
+]
+
 function App() {
   return (
     <div>
-      <ProductCard />
+      {
+        products.map((product) => 
+          <ProductCard data={product} />
+        )}
     </div>
   );
 }
--- src/components/ProductCard/index.jsx
+++ src/components/ProductCard/index.jsx
@@ -1,3 +1,17 @@
-export function ProductCard() {
-    return <div>Product Card</div>
+import "./index.css";
+
+export function ProductCard(props) {
+    // return <div>[props.data.name]</div>;
+
+    // const {image, category, name, price} = props.data
+    // это для краткости, чтобы не писать каждый раз props
+
+    return (
+        <div className="product-card">
+            <img width="200px" src={props.data.image} alt=""/>
+            <div>{props.data.category}</div>
+            <h4>{props.data.name}</h4>
+            <div>{props.data.price}</div>
+        </div>
+    )
 }
